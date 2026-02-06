6c256e439a0bebf348a7ce213738b6015fa54f78
LSUDOKO <adoranto737@gmail.com>
2026-02-07 01:13:46 +0530

    Fix: remove nested git repo and submodule

---
+++ web_server_go/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform() err %v", err)
+	}
+	fmt.Fprintf(w, "Post Request sucessfull")
+	name := r.FormValue("name")
+	Roll_NO := r.FormValue("Roll_NO")
+	Branch := r.FormValue("Branch")
+	Room_No := r.FormValue("Room_No")
+	fmt.Fprintf(w, "Name %s\n", name)
+	fmt.Fprintf(w, "Rollno %s\n", Roll_NO)
+	fmt.Fprintf(w, "Branch%s\n", Branch)
+	fmt.Fprintf(w, "Room No %s\n", Room_No)
+
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandlerFunc("/hello", helloHandler)
+	fmt.Println("Server start at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
