8e0c8003351be702433b081fac5caf84b24d51ce
LSUDOKO <adoranto737@gmail.com>
2026-03-04 18:46:08 +0530

    complete controllers

--- Books_Managment_Mysql/pkg/config/app.go
+++ Books_Managment_Mysql/pkg/config/app.go
@@ -10,8 +10,13 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:arpit/simplerest?")
+	d, err := gorm.Open("mysql", "root:arpit/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
+	db = d;
+
+}
+func GetDB() *gorm.DB {
+	return db;
 }
--- Books_Managment_Mysql/pkg/utils/utils.go
+++ Books_Managment_Mysql/pkg/utils/utils.go
@@ -1,3 +1,17 @@
 package utils
 
 //22.50
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func parseBody(r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+
+	}
+}
