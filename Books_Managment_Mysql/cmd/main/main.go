b116b9dc58a12dac80bb9797741a6d4f84ef9c20
LSUDOKO <adoranto737@gmail.com>
2026-03-04 18:45:35 +0530

    complete controllers

--- Books_Managment_Mysql/cmd/main/main.go
+++ Books_Managment_Mysql/cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
-
-import (
+import(
+	"net/http"
+	"log"
+	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/LSUDOKO/Go_Mini_Projects/tree/main/Books_Managment_Mysql/pkg/routes"
+
+
 )
+func main(){
+	r:=mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010",r ))
+}
